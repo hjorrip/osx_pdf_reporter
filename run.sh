79771dc9adfdfbe1d27c93ec40197be14a76f203
Hjortur Palmi Palsson <hjorrip@gmail.com>
2021-08-10 10:49:26 +0000

    dockerfile updated. shell file removed

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This shell scripts allows us to run the Docker container 
-# With the newest version of the source code, without having 
-# To rebuild the container after each change
-
-git pull
-python main.py
